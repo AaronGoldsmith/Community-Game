36f288b63d56dc6b721400c2f79cb9eb0a9fa035
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-18 10:05:46 -0700

    removed unused js

--- assets/author.js
+++ assets/author.js
@@ -1,88 +1,27 @@
 
 
   /* EVENT LISTENER ON THE USER STATE */
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      var emailVerified = user.emailVerified;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      // ...
-    }
-  });
+// firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       // User is signed in.
+//       var displayName = user.displayName;
+//       var email = user.email;
+//       var emailVerified = user.emailVerified;
+//       var isAnonymous = user.isAnonymous;
+//       var uid = user.uid;
+//       var providerData = user.providerData;
+//       firebase.database().ref("/users").push({
+//         user: displayName,
+//         email: email
+//       })
+//       console.log("you signed in")
+//     } else {
+//       // User is signed out.
+//       // ...
+//     }
+//   });
   
 
-
-  function toggleSignIn() {
-    if (firebase.auth().currentUser) {
-      // [START signout]
-      firebase.auth().signOut();
-      // [END signout]
-    } else {
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
-      if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-      }
-      if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-      }
-      // Sign in with email and pass.
-      // [START authwithemail]
-      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode === 'auth/wrong-password') {
-          alert('Wrong password.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        document.getElementById('quickstart-sign-in').disabled = false;
-        // [END_EXCLUDE]
-      });
-      // [END authwithemail]
-    }
-    document.getElementById('quickstart-sign-in').disabled = true;
-  }
-  /**
-   * Handles the sign up button press.
-   */
-  function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (email.length < 4) {
-      alert('Please enter an email address.');
-      return;
-    }
-    if (password.length < 4) {
-      alert('Please enter a password.');
-      return;
-    }
-    // Sign in with email and pass.
-    // Authenticate the user
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      if (errorCode == 'auth/weak-password') {
-        alert('The password is too weak.');
-      } else {
-        alert(errorMessage);
-      }
-      console.log(error);
-    });
-  }
   /**
    * Sends an email verification to the user.
    */
@@ -118,7 +57,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     // [START authstatelistener]
     firebase.auth().onAuthStateChanged(function(user) {
       // [START_EXCLUDE silent]
-      document.getElementById('quickstart-verify-email').disabled = true;
+      // document.getElementById('quickstart-verify-email').disabled = true;
       // [END_EXCLUDE]
       if (user) {
         // User is signed in.
@@ -130,9 +69,7 @@ firebase.auth().onAuthStateChanged(function(user) {
         var uid = user.uid;
         var providerData = user.providerData;
         // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-        document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
+        
         if (!emailVerified) {
           document.getElementById('quickstart-verify-email').disabled = false;
         }
@@ -140,13 +77,7 @@ firebase.auth().onAuthStateChanged(function(user) {
       } else {
         // User is signed out.
         // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-        document.getElementById('quickstart-account-details').textContent = 'null';
-        // [END_EXCLUDE]
+        
       }
-      // [START_EXCLUDE silent]
-      document.getElementById('quickstart-sign-in').disabled = false;
-      // [END_EXCLUDE]
     });
 }

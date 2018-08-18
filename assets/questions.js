e6952475f6fd3978143d8351ab506e41c437d3b3
eswilson <eswilson4@gmail.com>
2018-08-18 09:50:20 -0700

    index changes from yesterday, with demo script for adding questions and sketch canvas

---
+++ assets/questions.js
@@ -0,0 +1,25 @@
+var question = '';
+
+function addQuestion() {
+    // Create single row+column for new question
+    var newRow = $('<tr>');
+    var newCol = $('<td>');
+    // Create upvote+downvote arrows
+    var upVote = $('<i class="upvote fas fa-chevron-up"></i>');
+    var downVote = $('<i class="downvote fas fa-chevron-down"></i>');
+    var arrows = $('<div class="votes">');
+    arrows.append(upVote);
+    arrows.append(downVote);
+    newCol.append(question);
+    newRow.append(newCol);
+    newCol.prepend(arrows);
+    $('#tba-container').append(newRow);
+};
+
+$('#submit').on('click', function() {
+    event.preventDefault();
+    question = $('#question-input').val().trim();
+
+    addQuestion();
+    $('#question-input').val('');
+});

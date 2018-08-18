cbf84dc3f70639452e318e679de1975884b8092c
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-18 00:08:18 -0700

    began designing modal login

--- assets/script.js
+++ assets/script.js
@@ -1,47 +1,43 @@
-var config = {
-    apiKey: "AIzaSyAH20Zs8h38Z36SewkTWnVHLO3t7DsSG3g",
-    authDomain: "community-survey-c406d.firebaseapp.com",
-    databaseURL: "https://community-survey-c406d.firebaseio.com",
-    projectId: "community-survey-c406d",
-    storageBucket: "community-survey-c406d.appspot.com",
-    messagingSenderId: "765662643308"
-};
-
-firebase.initializeApp(config);
 var db = firebase.database();
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-var dbRef = db.ref();
-var uiConfig = {
-    callbacks: {
-        signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-        // User successfully signed in.
-        // Return type determines whether we continue the redirect automatically
-        // or whether we leave that to developer to handle.
-        return true;
-        },
-        uiShown: function() {
-          document.getElementById('loader').style.display = 'none';
-        }
-    },
-    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-    signInFlow: 'popup',
-    signInSuccessUrl: 'index.html',
-    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID, 
-                    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-                    firebase.auth.FacebookAuthProvider.PROVIDER_ID],
-  };
+
+$('#modalContainer').modal("show")                      // initialized with defaults
 
 $(document).ready(function(){
-    
-    $("#login").on("click",function(){
-      ui.start('#firebaseui-auth-container', uiConfig);
-      console.log($("#firebaseui-auth-container").child());
+
+   var uiConfig = {
+        callbacks: {
+            signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+            // User successfully signed in.
+            return true;
+            },
+            uiShown: function() {
+              document.getElementById('loader').style.display = 'none';
+            }
+        },
+        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+        signInFlow: 'popup',
+        signInSuccessUrl: 'index.html',
+        signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID, 
+                        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                        firebase.auth.FacebookAuthProvider.PROVIDER_ID],
+    };
+
+
+    $("#login").on("click",function()
+    {
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+      ui.start('#firebaseui-auth-container', {
+        signInFlow: 'popup',
+        signInSuccessUrl: '../index.html',
+        signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID, 
+                        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+                        firebase.auth.FacebookAuthProvider.PROVIDER_ID]
+      });
     })
 
-    // everytime page loads
-    dbRef.on("child_added", function(snapshot) {
-        console.log(snapshot)
-        dbRef.push({
+    // everytime page loads or value changes
+      db.ref().on("value", function(snapshot) {
+        db.ref().set({
             name: "test",
             email: "test@test.com",
             age: "20",
@@ -50,5 +46,6 @@ $(document).ready(function(){
     });
 
 });
+
 
 
